e38af78b56b8d71415a01154a767b7dc74652f3f
Dmitrii Evdokimov <ultra.las664@gmail.com>
2025-11-23 19:52:54 +0400

    Небольшие доработки по алертам

--- pages/alerts_page.py
+++ pages/alerts_page.py
@@ -3,6 +3,7 @@ from enum import StrEnum
 from pages.base_page import BasePage
 from selenium.common.exceptions import TimeoutException
 from utils.logs.logger import Logger
+from utils.random.random_factory import RandomFactory
 
 
 class AlertsData(StrEnum):
@@ -15,7 +16,6 @@ class AlertsData(StrEnum):
     CONFIRM_RESULT_TEXT = "You clicked: Ok"
 
     PROMPT_TEXT = "I am a JS prompt"
-    PROMPT_TEST_TEXT = "Dmitrii"
     PROMPT_RESULT_TEXT = "You entered: "
 
 
@@ -27,6 +27,7 @@ class AlertsPage(BasePage):
         self.unique_element = self.element(locator="//div[contains(@class, "
                                                    "'example')]//*[contains(text(), 'JavaScript Alerts')]",
                                            browser=browser)
+        self.random_factory = RandomFactory()
 
     def open(self):
         try:
@@ -38,6 +39,9 @@ class AlertsPage(BasePage):
 
     def get_result_element_text(self):
         return self.element(locator="//*[@*='result']", browser=self.browser).get_text()
+
+    def get_random_string(self):
+        return self.random_factory.get_random_string()
 
     def click_js_alert_button(self) -> None:
         self.element(locator="//button[@onclick = 'jsAlert()']", browser=self.browser).click()
@@ -55,7 +59,7 @@ class AlertsPage(BasePage):
             if AlertsData.ALERT_TEXT == self.browser.get_alert_text():
                 self.browser.confirm_alert()
                 if self.get_result_element_text() == AlertsData.RESULT_ALERT_TEXT:
-                    Logger.info(f"Check JavaScript alert successful: {self.get_result_element_text()} == {AlertsData.RESULT_ALERT_TEXT}")
+                    Logger.info(f"Check JavaScript alert successful: '{self.get_result_element_text()}' == '{AlertsData.RESULT_ALERT_TEXT}'")
                     return True
         except Exception as err:
             Logger.error(f"Check JavaScript alert failed: {err}")
@@ -68,7 +72,7 @@ class AlertsPage(BasePage):
             if AlertsData.CONFIRM_TEXT == self.browser.get_alert_text():
                 self.browser.confirm_alert()
                 if self.get_result_element_text() == AlertsData.CONFIRM_RESULT_TEXT:
-                    Logger.info(f"Check JavaScript confirm successful: {self.get_result_element_text()} == {AlertsData.CONFIRM_RESULT_TEXT}")
+                    Logger.info(f"Check JavaScript confirm successful: '{self.get_result_element_text()}' == '{AlertsData.CONFIRM_RESULT_TEXT}'")
                     return True
         except Exception as err:
             Logger.error(f"Check JavaScript confirm failed: {err}")
@@ -78,13 +82,14 @@ class AlertsPage(BasePage):
         try:
             self.click_js_prompt_button()
             self.browser.switch_to_alert()
+            random_string = self.get_random_string()
             if AlertsData.PROMPT_TEXT == self.browser.get_alert_text():
-                self.browser.send_keys(AlertsData.PROMPT_TEST_TEXT)
+                self.browser.send_keys(random_string)
                 self.browser.confirm_alert()
-                if self.get_result_element_text() == (AlertsData.PROMPT_RESULT_TEXT + AlertsData.PROMPT_TEST_TEXT):
-                    Logger.info(f"Check JavaScript prompt successful: {self.get_result_element_text()}"
-                                f"== {(AlertsData.PROMPT_RESULT_TEXT + AlertsData.PROMPT_TEST_TEXT)}")
-                    return Tru
+                if self.get_result_element_text() == (AlertsData.PROMPT_RESULT_TEXT + random_string):
+                    Logger.info(f"Check JavaScript prompt successful: '{self.get_result_element_text()}'"
+                                f" == '{(AlertsData.PROMPT_RESULT_TEXT + random_string)}'")
+                    return True
         except Exception as err:
             Logger.error(f"Check JavaScript prompt failed: {err}")
             return False
---
+++ utils/random/random_factory.py
@@ -0,0 +1,8 @@
+import string
+import random
+
+class RandomFactory:
+    STRING_LENGTH = 10
+    def get_random_string(self):
+        return "".join(random.choice(string.ascii_letters) for i in range(self.STRING_LENGTH))
+
