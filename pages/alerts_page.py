88c67d15f1522544acaf6fccc2daa2e184464167
Dmitrii Evdokimov <ultra.las664@gmail.com>
2026-01-12 01:04:22 +0400

    Рефакторинг тестов и страницы с алертами

--- pages/alerts_page.py
+++ pages/alerts_page.py
@@ -7,17 +7,6 @@ from utils.random.random_factory import RandomFactory
 from elements.custom_elements.label import Label
 
 
-class TestAlertsData(StrEnum):
-    ALERT_TEXT = "I am a JS Alert"
-    ALERT_RESULT_TEXT = "You successfully clicked an alert"
-
-    CONFIRM_TEXT = "I am a JS Confirm"
-    CONFIRM_RESULT_TEXT = "You clicked: Ok"
-
-    PROMPT_TEXT = "I am a JS prompt"
-    PROMPT_RESULT_TEXT = "You entered: "
-
-
 class AlertsPage(BasePage):
     ALERT_PAGE_UNIQUE_LOC = "//div[contains(@class,'example')]//*[contains(text(), 'JavaScript Alerts')]"
     ALERT_RESULT_TEXT = "//*[@*='result']"
@@ -40,7 +29,7 @@ class AlertsPage(BasePage):
         except TimeoutException:
             return False
 
-    def get_result_element_text(self):
+    def get_result_text(self):
         return self.element(locator=self.ALERT_RESULT_TEXT).get_text()
 
     def get_random_string(self):
@@ -54,66 +43,3 @@ class AlertsPage(BasePage):
 
     def click_js_prompt_button(self) -> None:
         self.element(locator=self.JS_PROMPT_BUTTON).click()
-
-    def check_alert_text(self, text: str):
-        correct_text = self.browser.get_alert_text()
-        try:
-            if text == correct_text:
-                Logger.info(f"Alert '{text}' is correct for '{correct_text}'")
-                return True
-        except Exception as err:
-            Logger.error(f"Alert '{text}' is incorrect for '{correct_text}': {err}")
-            return False
-
-    def check_result_text(self, text):
-        correct_text = self.get_result_element_text()
-        try:
-            if text == correct_text:
-                Logger.info(f"Result '{text}' is correct for {correct_text}")
-                return True
-        except Exception as err:
-            Logger.error(f"Result '{text}' is incorrect for '{correct_text}': {err}")
-
-    def check_alert(self):
-        try:
-            self.click_js_alert_button()
-            self.browser.switch_to_alert()
-            if self.check_alert_text(TestAlertsData.ALERT_TEXT):
-                self.browser.confirm_alert()
-                if self.check_result_text(TestAlertsData.ALERT_RESULT_TEXT):
-                    Logger.info(
-                        f"Check JavaScript alert successful: '{self.get_result_element_text()}' == '{TestAlertsData.ALERT_RESULT_TEXT}'")
-                    return True
-        except Exception as err:
-            Logger.error(f"Check JavaScript alert failed: {err}")
-            return False
-
-    def check_confirm(self):
-        try:
-            self.click_js_confirm_button()
-            self.browser.switch_to_alert()
-            if self.check_alert_text(TestAlertsData.CONFIRM_TEXT):
-                self.browser.confirm_alert()
-                if self.check_result_text(TestAlertsData.CONFIRM_RESULT_TEXT):
-                    Logger.info(
-                        f"Check JavaScript confirm successful: '{self.get_result_element_text()}' == '{TestAlertsData.CONFIRM_RESULT_TEXT}'")
-                    return True
-        except Exception as err:
-            Logger.error(f"Check JavaScript confirm failed: {err}")
-            return False
-
-    def check_prompt(self):
-        try:
-            self.click_js_prompt_button()
-            self.browser.switch_to_alert()
-            random_string = self.get_random_string()
-            if self.check_alert_text(TestAlertsData.PROMPT_TEXT):
-                self.browser.send_keys_alert(random_string)
-                self.browser.confirm_alert()
-                if self.check_result_text(TestAlertsData.PROMPT_RESULT_TEXT + random_string):
-                    Logger.info(f"Check JavaScript prompt successful: '{self.get_result_element_text()}'"
-                                f" == '{(TestAlertsData.PROMPT_RESULT_TEXT + random_string)}'")
-                    return True
-        except Exception as err:
-            Logger.error(f"Check JavaScript prompt failed: {err}")
-            return False
--- tests/test_alerts.py
+++ tests/test_alerts.py
@@ -1,6 +1,18 @@
 from pages.alerts_page import AlertsPage
 from pages.js.alerts_js import AlertsJS
 from config.urls import URLs
+from enum import StrEnum
+
+
+class TestAlertsData(StrEnum):
+    ALERT_TEXT = "I am a JS Alert"
+    ALERT_RESULT_TEXT = "You successfully clicked an alert"
+
+    CONFIRM_TEXT = "I am a JS Confirm"
+    CONFIRM_RESULT_TEXT = "You clicked: Ok"
+
+    PROMPT_TEXT = "I am a JS prompt"
+    PROMPT_RESULT_TEXT = "You entered: "
 
 
 class TestAlerts:
@@ -9,11 +21,22 @@ class TestAlerts:
         alerts_page = AlertsPage(browser)
         browser.get(URLs.ALERTS_PAGE)
         assert alerts_page.open(), "Ошибка при открытии страницы с алертами \n"
-        assert alerts_page.check_alert(), "Ошибка при проверке алерта \n"
-        assert alerts_page.check_confirm(), "Ошибка при проверке алерта с подтверждением \n"
-        assert alerts_page.check_prompt(), "Ошибка при проверке алерта с полем и отправкой значения \n"
 
-        alerts_js = AlertsJS(browser)
-        assert alerts_js.check_alert_js(), "Ошибка при проверке алерта с JavaScript \n"
-        assert alerts_js.check_confirm_js(), "Ошибка при проверке алерта с подтверждением с JavaScript \n"
-        assert alerts_js.check_prompt_js(), "Ошибка при проверке алерта с полем и отправкой значения с JavaScript \n"
+        alerts_page.click_js_alert_button()
+        assert browser.get_alert_text() == TestAlertsData.ALERT_TEXT, "Ошибка при проверке текста алерта \n"
+        self.browser.confirm_alert()
+        assert browser.get == TestAlertsData.ALERT_RESULT_TEXT, "Ошибка при проверке результата текста алерта"
+
+        alerts_page.click_js_confirm_button()
+        self.browser.switch_to_alert()
+        assert browser.get_alert_text() == TestAlertsData.CONFIRM_TEXT, "Ошибка при проверке текста алерта типа Confirm"
+        self.browser.confirm_alert()
+        assert alerts_page.get_result_text() == TestAlertsData.CONFIRM_RESULT_TEXT, "Ошибка при проверке результата алерта типа Confirm"
+
+        alerts_page.click_js_prompt_button()
+        self.browser.switch_to_alert()
+        random_string = alerts_page.get_random_string()
+        assert browser.get_alert_text() == TestAlertsData.PROMPT_TEXT, "Ошибка при проверке текста алерта типа Prompt"
+        self.browser.send_keys_alert(random_string)
+        self.browser.confirm_alert()
+        assert alerts_page.get_result_text() == TestAlertsData.PROMPT_RESULT_TEXT, "Ошибка при проверке результата алерта типа Prompt"
